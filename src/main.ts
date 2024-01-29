4db62c4791c5ca2b3bae9a389a7b2efc7df85254
foosint <foobi_owl@riseup.net>
2024-01-30 00:44:35 +0100

    fix: remove labels on search - part 1

--- src/main.ts
+++ src/main.ts
@@ -967,8 +967,12 @@ class MapViewer {
             .includes(current_search_string)
         ) {
           elem?.classList.add('hide');
+          const tooltipClass = elem.getAttribute('aria-describedby');
+          document.getElementById(tooltipClass)?.classList.add('hide');
         } else {
           elem?.classList.remove('hide');
+          const tooltipClass = elem.getAttribute('aria-describedby');
+          document.getElementById(tooltipClass)?.classList.remove('hide');          
         }
       });
     });
